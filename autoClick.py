f639bbdf57d98811871291ff0ae81ca80785404a
reynarvaez <reynaldo_narvaez@hotmail.com>
2019-02-04 19:03:35 -0600

    optimized code and fixed two block in the same column bug

--- autoClick.py
+++ autoClick.py
@@ -1,41 +1,59 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
-from directKeys import click, queryMousePosition, W, moveMouseTo
+from directKeys import click, holdClick, releaseClick, queryMousePosition, W, moveMouseTo
 import time
 
-#debug method to read black blocks
+
+# debug method to show the whole screen
+def showScreen():
+    screen = np.array(ImageGrab.grab(bbox=gameCoords))
+    screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('screen', screen)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# debug method to show screen
+def showScreenWithCoordinates(coordinates):
+    screen = np.array(ImageGrab.grab(bbox=coordinates))
+    screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('screen', screen)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# debug method to read black blocks
 def readBlack(Screen, Coordinates):
     for y in reversed(range(len(Screen))):
         for x in range(len(Screen[y])):
             if Screen[y][x] < 45:
                 moveMouseTo(x + round(Coordinates[0]), y + round(Coordinates[1]))
                 time.sleep(0.001)
-    # moveMouseTo(x + round(Coordinates[0]), y + round(Coordinates[1]))
-    # time.sleep(0.5)
+            # print(Screen[y][x])
+            # moveMouseTo(x + round(Coordinates[0]), y + round(Coordinates[1]))
+            # time.sleep(0.001)
 
-#Clicks the first blackk pixel it finds
-def clickBlack(Screen, Coordinates):
+# Clicks the first black pixel it finds
+def clickBlack(Screen, Coordinates, Column):
     for y in reversed(range(len(Screen))):
         for x in range(len(Screen[y])):
             if Screen[y][x] < 45:
-                click(x + round(Coordinates[0]), y + round(Coordinates[1]))
+                releaseClick()
+                holdClick(x + round(Coordinates[0]), y + round(Coordinates[1]))
                 return
 
-#trims columns to read just bits of the columns instead of the whole screen, boosting efficiency
+# trims columns to read just bits of the columns instead of the whole screen, boosting efficiency
 def trimColums(coords):
     standardColumnLength = 1
     x1 = coords[0]
     halfBlock = blockLength/2
-    x1 = x1 + halfBlock - round(standardColumnLength/2)
+    x1 = x1 + round(halfBlock) - round(standardColumnLength / 2)
     x2 = x1 + round(standardColumnLength)
     coords[0] = x1
     coords[2] = x2
     return coords
 
-
-#reads the screen and gets all the locations of the black blocks and sorts them from lower(in the screen) to highest
-def getLowestBlack():
+# reads the screen and gets all the locations of the black blocks and sorts them from lower(in the screen) to highest
+def getLowestBlack(counter):
     highestPixel = 10000
     prevhighestPixel = 10000
     sortedBlacks = []
@@ -52,65 +70,96 @@ def getLowestBlack():
         coordinates = trimColums(coordinates)
         screen = np.array(ImageGrab.grab(bbox=coordinates))
         screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('screen', screen)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         # readBlack(screen, coordinates)
         prevhighestPixel = highestPixel
-        highestPixel = getHighestPixel(screen, coordinates)
+        blocks = getHighestPixel(screen, coordinates)
 
-        sortedBlacks.append(highestPixel)
-        columnBlacks.append(i)
-        indexBlacks.append(highestPixel)
-        print("columns: " + str(i + 1) + " prevhighestPixel: " + str(prevhighestPixel) + " highestPixel: " + str(
-            highestPixel))
+        # if blocks.__len__() > 2:
+        #     cv2.imwrite(str(i) + "_column" + str(i) + ".jpg",
+        #                 cv2.cvtColor(np.array(ImageGrab.grab(bbox=gameCoords)), cv2.COLOR_BGR2GRAY))
 
-        # greater than because the greater the pixel the lowest it's in the screen
+        for index in range(len(blocks)):
+            sortedBlacks.append(blocks[index])
+            columnBlacks.append(i)
+            indexBlacks.append(blocks[index])
+            print("columns: " + str(i + 1) + " prevhighestPixel: " + str(prevhighestPixel) + " highestPixel: " + str(
+                blocks[index]))
+
     sortedBlacks.sort(reverse=True)
 
-    for i in list(range(4))[::+1]:
+    for i in range(len(sortedBlacks)):
         if sortedBlacks[i] != 10000:
             finalOrder.append(columnBlacks[indexBlacks.index(sortedBlacks[i])])
         else:
             finalOrder.append(-1)
 
+    finalOrder = cleanOrder(finalOrder)
+
     return finalOrder
 
-#gets the highest black pixel(block) in each column
+# The game can have two black blocks on the same column so we want to remove the second block from our ordered list
+def cleanOrder(columns):
+    prevColumn = -1
+    currentColumn = -1
+    popCount = 0
+    for i in range(len(columns)):
+        i = i - popCount
+        currentColumn = columns[i]
+        if currentColumn == prevColumn:
+            columns.pop(i)
+            popCount += 1
+        else:
+            prevColumn = currentColumn
+    return columns
+
+
+# gets the highest black pixel(block) in each column
 def getHighestPixel(Screen, Coordinates):
+    blocks = []
     highestPixel = 10000
+    lastHigh = 10000
+    currHigh = 10000
+    switch = True
+    prevHighestPixel = -10000
 
     for y in range(len(Screen)):
         for x in range(len(Screen[y])):
             if Screen[y][x] < 45:
+
+                if not switch:
+                    break
+
                 # moveMouseTo(x + round(Coordinates[0]), y + round(Coordinates[1]))
                 # time.sleep(1)
                 highestPixel = y + round(Coordinates[1])
-                return highestPixel
+
+                if switch:
+
+                    if highestPixel > (prevHighestPixel + blockHeight):
+                        blocks.append(highestPixel)
+                        switch = False
+                        prevHighestPixel = highestPixel
+            else:
+                # print(Screen[y][x])
+                switch = True
+
         # moveMouseTo(x + round(Coordinates[0]), y + round(Coordinates[1]))
         # time.sleep(0.001)
-    return highestPixel
+    return blocks
 
 
 def processColumns(counter):
-    columns = getLowestBlack()
+    columns = getLowestBlack(counter)
     print("sortedColumns: " + str(columns))
 
-    # if all columns return the value 10000 means there's no block in sight
-    allNegative = columns.count(10000) == 4
-
-    # if all columns return the value 0 means the whole screen is black and it's game over
-    allZeros = columns.count(0) == 4
-
-    if counter > 4:
-        counter = 0
-
-    if allNegative:
-        print("------------------------------------WAITING FOR BLOCK TO COME DOWN-------------------------------------")
-        return False
-
-    if allZeros:
+    if columns.__len__() > 7:
         print("------------------------------------STOP PROGRAM-------------------------------------")
         return True
 
-    for i in list(range(4))[::+1]:
+    for i in range(len(columns)):
 
         if columns[i] == -1:
             print("Skip Column: " + str(columns[i]))
@@ -124,9 +173,16 @@ def processColumns(counter):
         y2 = gameCoords[3]
         coordinates = [x1, y1, x2, y2]
         coordinates = trimColums(coordinates)
+        # print(coordinates)
         screen = np.array(ImageGrab.grab(bbox=coordinates))
         screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-        clickBlack(screen, coordinates)
+        # cv2.imwrite(str(counter) + "_column" + str(i) + "_screen.jpg", screen)
+        # cv2.imwrite(str(counter) + "_column" + str(i) + ".jpg", cv2.cvtColor(np.array(ImageGrab.grab(bbox=gameCoords)), cv2.COLOR_BGR2GRAY))
+        # cv2.imshow('screen', screen)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # readBlack(screen, coordinates)
+        clickBlack(screen, coordinates, columns[i])
 
 
 def Start():
@@ -141,7 +197,7 @@ def Start():
     elapsed = 0
     stopProgram = False
     counter = 0
-    while elapsed < 10:
+    while elapsed < 600:
 
         stopProgram = processColumns(counter)
 
@@ -159,11 +215,11 @@ print(gameCoords)
 gameLength = gameCoords[2] - gameCoords[0]
 gameHeight = gameCoords[3] - gameCoords[1]
 
-#Each black block 
+# Each black block 
 blockLength = gameLength/4
 blockHeight = gameHeight/4
 
-#Columns corresponding to each block
+# Columns corresponding to each block
 column1 = [0, blockLength]
 column2 = [column1[1], blockLength*2]
 column3 = [column2[1], blockLength*3]
@@ -173,3 +229,4 @@ gameColumns = [column1, column2, column3, column4]
 print(gameColumns)
 
 Start()
+# showScreen()
